a24637dadc11d58eb249519f5e11142a8a8e20a1
ArsanCodifire <132670256+ArsanCodifire@users.noreply.github.com>
2024-08-05 20:11:41 +0530

    Update db.py

--- db.py
+++ db.py
@@ -1,27 +1,35 @@
 import discord as ds
 from discord.ext import commands as cmd
 import streamlit as st
-import time
-token=st.secrets["TOKEN"]
-bot=cmd.Bot(command_prefix="/",intents=ds.Intents.all())
+import threading
+
+token = st.secrets["TOKEN"]
+bot = cmd.Bot(command_prefix="/", intents=ds.Intents.all())
+
 st.set_page_config(
     page_title="Pybot",
-    page_icon ="🤖"
+    page_icon="🤖"
 )
+
 st.title("Discord Pybot")
+
 @bot.event
 async def on_ready():
-    st.write(f'Logged in as {bot.user}')
+    print(f'Logged in as {bot.user}')  # Use print instead of st.write here
 
 @bot.command()
 async def hi(ctx):
-    if not ctx.author==bot.user:
+    if ctx.author != bot.user:
         await ctx.send(f'Hi {ctx.author.mention}!')
-        time.sleep(15)
-        st.write("Used /hi func")
-    else:
-        no=1
+        print("Used /hi func")  # Use print instead of st.write here
 
-bot.run(token)
+def run_bot():
+    bot.run(token)
 
+# Streamlit setup
+if st.button("Start Bot"):
+    st.write("Starting the bot...")
+    bot_thread = threading.Thread(target=run_bot, daemon=True)
+    bot_thread.start()
 
+st.write("Bot is running. Type /hi in your Discord server to test.")

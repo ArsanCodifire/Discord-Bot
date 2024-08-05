3175599aae5ffc7a3c0284b2a6b6aeb3a06ba581
ArsanCodifire <132670256+ArsanCodifire@users.noreply.github.com>
2024-08-05 19:47:43 +0530

    Update db.py

--- db.py
+++ db.py
@@ -1,7 +1,6 @@
 import discord
 from discord.ext import commands
 import streamlit as st
-import threading
 token=st.secrets["TOKEN"]
 bot=cmd.Bot(command_prefix="/",intents=ds.Intents.all())
 st.set_page_config(
@@ -15,20 +14,9 @@ async def on_ready():
 
 @bot.command()
 async def hi(ctx):
+    st.write("Used /hi func")
     await ctx.send(f'Hi {ctx.author.mention}!')
 
-def run_bot():
-    bot.run(token)
-
-# Streamlit setup
-def main():
-    st.write("Starting the bot...")
-    bot_thread = threading.Thread(target=run_bot)
-    bot_thread.start()
-
-    st.write("Bot is running. Type /hi in your Discord server to test.")
-
-if __name__ == "__main__":
-    main()
+bot.run(token)
 
 

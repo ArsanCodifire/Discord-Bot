1127f1e96ba717b12f92e1f5ddb3f37d36ae5bb6
ArsanCodifire <132670256+ArsanCodifire@users.noreply.github.com>
2024-08-05 20:27:37 +0530

    Update db.py

--- db.py
+++ db.py
@@ -1,11 +1,13 @@
 import discord as ds
 from discord.ext import commands as cmd
 import streamlit as st
-import threading
+import multiprocessing
 
+# Token setup
 token = st.secrets["TOKEN"]
 bot = cmd.Bot(command_prefix="/", intents=ds.Intents.all())
 
+# Streamlit page configuration
 st.set_page_config(
     page_title="Pybot",
     page_icon="🤖"
@@ -13,23 +15,24 @@ st.set_page_config(
 
 st.title("Discord Pybot")
 
+# Discord bot events and commands
 @bot.event
 async def on_ready():
-    print(f'Logged in as {bot.user}')  # Use print instead of st.write here
+    print(f'Logged in as {bot.user}')  # Print to console instead of st.write
 
 @bot.command()
 async def hi(ctx):
     if ctx.author != bot.user:
         await ctx.send(f'Hi {ctx.author.mention}!')
-        print("Used /hi func")  # Use print instead of st.write here
+        print("Used /hi func")  # Print to console instead of st.write
 
 def run_bot():
     bot.run(token)
 
-# Streamlit setup
-if st.button("Start Bot"):
-    st.write("Starting the bot...")
-    bot_thread = threading.Thread(target=run_bot, daemon=True)
-    bot_thread.start()
+# Start the bot process
+if __name__ == "__main__":
+    bot_process = multiprocessing.Process(target=run_bot)
+    bot_process.start()
 
-st.write("Bot is running. Type /hi in your Discord server to test.")
+    st.write("Pybot is running.")
+

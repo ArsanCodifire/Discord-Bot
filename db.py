a917ef0c297a7af3d5498e0880750423311b0e6c
ArsanCodifire <132670256+ArsanCodifire@users.noreply.github.com>
2024-08-05 17:26:50 +0530

    Create db.py

---
+++ db.py
@@ -0,0 +1,14 @@
+import streamlit as st
+from discord.ext import commands as cmd
+import discord as ds
+token=st.seceret["TOKEN"]
+bot=cmd.Bot(command_prefix="/",intents=ds.Intents.all())
+@bot.event
+async def main():
+    st.write("Discord Pybot")
+    st.write(f"Logged as {bot.user}")
+@bot.command()
+async def hello(ctk):
+    ctk.send(f"Hi {ctx.author.mention}")
+    st.write("Used /hi func")
+bot.run(token)
